850c1c7f76dc46400a8055fa763db2ca5c453ed4
baddam619 <95213635+baddam619@users.noreply.github.com>
2024-01-07 06:54:24 +0530

    main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print('suhanth')
-input('what is your name?')
-name = input('what is your name?')
-print(name)
+input('what is your name?')  // suhanth
+name = input('what is your name?')  // input as suhanth
+print(name)  // o/p as suhanth
